4350305cf5ff99e0ecbf2f5e9fc3226a3cd9b1ed
Albert Hopkins <marduk@python.net>
2011-12-13 17:50:17 -0500

    Change memdraggable plugin so it does not move windows out of bounds

--- djangoproject/airport/templates/airport/common.js
+++ djangoproject/airport/templates/airport/common.js
@@ -7,11 +7,34 @@
         var id = this.attr('id');
         var coords = this.offset();
         var split, x, y;
+        var window_width, window_height;
 
         this.draggable();
 
         this.bind('dragstop', function() {
             var coords = $(this).offset();
+            var window_width, window_height;
+
+            window_width = $(window).width();
+            window_height = document.documentElement.clientHeight;
+            /* $(window).height() broken in webkit see:
+             * http://forum.jquery.com/topic/window-height-in-safari-is-incorrect
+             */
+
+            /* ensure we're within bounds */
+            if (coords.top < 0) {
+                coords.top = 0;
+            }
+            if (coords.top > window_height) {
+                coords.top = window_height;
+            }
+            if (coords.left < 0) {
+                coords.left = 0;
+            }
+            if (coords.left > window_width) {
+                coords.left = window_width;
+            }
+
             $(this).css('opacity', $(this).data('opacity'));
             $(this).css('z-index', 0);
             $.cookie(
@@ -29,10 +52,32 @@
 
         /* load position */
         coords = $.cookie(id + '_pos');
+        window_width = $(window).width();
+        window_height = document.documentElement.clientHeight;
         if (coords != null) {
             split = coords.split(':', 2);
             x = split[0];
             y = split[1];
+
+            window_width = $(window).width();
+            window_height = document.documentElement.clientHeight;
+            /* $(window).height() broken in webkit see:
+             * http://forum.jquery.com/topic/window-height-in-safari-is-incorrect
+             */
+
+            /* ensure we're within bounds */
+            if (y < 1) {
+                y = 1;
+            }
+            if (y > window_height) {
+                y = window_height;
+            }
+            if (x < 1) {
+                x = 1;
+            }
+            if (x > window_width) {
+                x = window_width;
+            }
             this.offset({ left: x, top: y});
         }
     };
