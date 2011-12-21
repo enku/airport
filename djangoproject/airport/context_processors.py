3ebbdcb2d4838f05ead86b720b680a89e6f7faea
Albert Hopkins <marduk@python.net>
2011-12-21 14:38:42 -0500

    context_processors.py: Don't use SPACE character in URLs

--- djangoproject/airport/context_processors.py
+++ djangoproject/airport/context_processors.py
@@ -14,21 +14,21 @@ EXTERNALS = {
     'register_icon': 'https://help.ess.msu.edu/themes/client_default/063684-green-metallic-orb-icon-people-things-handshake.png',
     'background_image': 'http://images2.layoutsparks.com/1/238605/moon-over-mistral-sky.jpg',
     'inflight_image': 'http://av-mech.com/images/flyinganimated_20airplane.gif',
-    'landed_sound': 'http://soundbible.com/mp3/757 Landing-SoundBible.com-1539957439.mp3',
-    'takeoff_sound': 'http://soundbible.com/mp3/757 Landing-SoundBible.com-1539957439.mp3',
+    'landed_sound': 'http://soundbible.com/mp3/757%20Landing-SoundBible.com-1539957439.mp3',
+    'takeoff_sound': 'http://soundbible.com/mp3/757%20Landing-SoundBible.com-1539957439.mp3',
     'gold_star': 'http://njwltech.wikispaces.com/file/view/gold_star.png/35350229/gold_star.png',
     'inbox_icon': 'http://starsvet.com/templates/tmpl_ae4/images_ae4/write_message.gif',
     'inbox_sound': 'http://soundbible.com/mp3/A-Tone-His_Self-1266414414.mp3',
-    'ticket_sound': 'http://soundbible.com/mp3/Cash Register Cha Ching-SoundBible.com-184076484.mp3',
+    'ticket_sound': 'http://soundbible.com/mp3/Cash%20Register%20Cha%20Ching-SoundBible.com-184076484.mp3',
     'finished_image': 'http://www.moodiereport.com/images2/munich_airport_600px_dec09.jpg',
     'notification_icon': 'http://icons.iconarchive.com/icons/icons-land/points-of-interest/64/Airport-Blue-icon.png',
 
     # messages
     'message_MONKEYWRENCH_sound': 'http://www.freesound.org/data/previews/17/17468_57789-lq.mp3',
     'message_DEFAULT_sound': 'http://soundbible.com/mp3/A-Tone-His_Self-1266414414.mp3',
-    'message_GOAL_sound': 'http://soundbible.com/mp3/Kids Cheering-SoundBible.com-681813822.mp3',
+    'message_GOAL_sound': 'http://soundbible.com/mp3/Kids%20Cheering-SoundBible.com-681813822.mp3',
     'message_GAMEOVER_sound': 'http://www.freesound.org/data/previews/42/42349_81909-lq.mp3',
-    'message_WINNER_sound': 'http://soundbible.com/mp3/Ta Da-SoundBible.com-1884170640.mp3'
+    'message_WINNER_sound': 'http://soundbible.com/mp3/Ta%20Da-SoundBible.com-1884170640.mp3'
 }
 
 def externals(request):
