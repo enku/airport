f1f9021d331f0f161dbbce38b1eea21b94ed89d5
Albert Hopkins <marduk@python.net>
2012-01-02 16:06:26 -0500

    Fix broken messages JS

--- djangoproject/airport/templates/airport/messages.js
+++ djangoproject/airport/templates/airport/messages.js
@@ -2,28 +2,28 @@ var airport = window.airport || {};
 
 airport.messages = function(widget) {
 
-    this.append = function(data) {
+    var update = function() {
+        if (initial) {
+            $.get('{% url messages %}', {last: last_message, old: 'true'}, 
+                  append);
+            initial = false;
+            return;
+        }
+        $.get('{% url messages %}', {last: last_message}, append);
+    }
+
+    var append = function(data) {
         if (data != '') {
             widget.append(data);
             last_message = widget.data('last_message');
         }
-        setTimeout(this.update, 5000);
-    }
-
-    this.update = function() {
-        if (initial) {
-            $.get('{% url messages %}', {last: last_message, old: 'true'}, 
-                  this.append);
-            initial = false;
-            return;
-        }
-        $.get('{% url messages %}', {last: last_message}, this.append);
+        setTimeout(update, 5000);
     }
 
     var last_message = '0';
     var widget = $(widget);
     var initial = true;
 
-    this.update();
+    update();
     return this;
 }
